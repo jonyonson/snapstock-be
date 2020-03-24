0707d759a525669678a7c936518905538f7114ec
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-03-23 21:53:11 -0500

    create users table

---
+++ data/migrations/20200323214745_users.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('users', (t) => {
+    t.increments();
+    t.string('email')
+      .notNullable()
+      .unique();
+    t.string('password').notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('users');
+};
