06d2ec147e3ede886e1b499fd26b110abaf53a93
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-04-11 12:22:20 -0500

    add check to ensure 6 stories are included in news endpoint

--- api/news/news-router.js
+++ api/news/news-router.js
@@ -7,6 +7,7 @@ const NEWS_API_KEY = process.env.NEWS_API_KEY;
 router.get('/top-headlines', async (req, res) => {
   const url = `http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${NEWS_API_KEY} `;
 
+  // Preferred sites
   const whitelist = [
     'CNBC',
     'Trust.org',
@@ -21,13 +22,33 @@ router.get('/top-headlines', async (req, res) => {
     'CNN',
   ];
 
+  // These sites consistenly have issues with images, link or content
+  const blacklist = ['Bloomberg', 'Nypost.com'];
+
   try {
     const response = await fetch(url);
     let data = await response.json();
 
+    // get all of the articles from the whitelist first
     const articles = data.articles.filter((article) => {
       return whitelist.includes(article.source.name);
     });
+
+    // add additional articles until total is 6
+    // as long as the source isn't blacklisted
+    let i = 0;
+    while (articles.length < 6) {
+      if (
+        !articles.includes(data.articles[i]) &&
+        !blacklist.includes(data.articles[i].source.name)
+      ) {
+        articles.push(data.articles[i]);
+      }
+      i++;
+    }
+
+    // get the articles back in order
+    articles.sort((a, b) => a.publishedAt - b.publishedAt);
 
     res.status(200).json(articles);
   } catch (err) {
