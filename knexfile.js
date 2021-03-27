cab77fab51851ead8b1286f8657e1c4f86408bc1
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2021-03-27 13:44:12 -0500

    update local db settings

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,7 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: {
-      database: process.env.DATABASE,
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
@@ -30,23 +28,4 @@ module.exports = {
       directory: './data/seeds',
     },
   },
-
-  // development: {
-  //   client: 'sqlite3',
-  //   useNullAsDefault: true,
-  //   connection: {
-  //     filename: './data/dev.sqlite3',
-  //   },
-  //   migrations: {
-  //     directory: './data/migrations',
-  //   },
-  //   seeds: {
-  //     directory: './data/seeds',
-  //   },
-  //   pool: {
-  //     afterCreate: (conn, done) => {
-  //       conn.run('PRAGMA foreign_keys = ON', done);
-  //     },
-  //   },
-  // },
 };
