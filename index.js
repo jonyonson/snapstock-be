08e8bd439c4ee38d97f54fe31673dc2c13e0c36d
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2022-02-23 05:40:41 -0600

    fix: whitelist

--- index.js
+++ index.js
@@ -14,14 +14,8 @@ const port = process.env.PORT || 5000;
 
 const whitelist = [
   'https://snapstockapp.com',
-  'https://snapstock.vercel.app/',
-  'https://snapstock-mulligan.vercel.app/',
-  'http://localhost:3000',
-  'http://localhost:3001',
-  'http://localhost:6006',
-  'http://10.0.0.39:6006',
-  'http://10.0.0.39:3000',
-  'http://localhost:8001',
+  'https://snapstock.vercel.app',
+  'https://snapstock-mulligan.vercel.app',
 ];
 
 const corsOptions = {
