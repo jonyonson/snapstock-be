c315f6107d53521c6128b92286ae07d5807076ce
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2022-11-13 11:36:13 -0600

    fix: production config

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 const stage = process.env.STAGE || 'development';
 
 let envConfig;
-if (stage === 'development') {
+if (stage === 'production') {
   envConfig = require('./production').default;
 } else {
   envConfig = require('./development').default;
