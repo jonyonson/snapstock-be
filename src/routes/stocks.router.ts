5ed5d6aa521a3af4286c6e963e97b244272092d7
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2022-11-15 11:35:16 -0600

    feat: add market movers endpoint

--- src/routes/stocks.router.ts
+++ src/routes/stocks.router.ts
@@ -1,8 +1,39 @@
 import express from 'express';
-import { MARKET_INDEX_URL } from '../utils/constants';
+import { IEX_CLOUD, MARKET_INDEX_URL } from '../utils/constants';
 import fetch from 'node-fetch';
 
 const router = express.Router();
+
+/**
+ * 'mostactive' | 'gainers' | 'losers' |'iexvolume' | 'iexpercent'
+ * */
+router.get('/market/list/:type', async (req, res, next) => {
+  const { type } = req.params;
+
+  const listTypes = [
+    'mostactive',
+    'gainers',
+    'losers',
+    'iexvolume',
+    'iexpercent',
+  ];
+
+  if (!listTypes.includes(type)) {
+    const message = `Must be a valid list type ('${listTypes.join("', '")})`;
+    return res.status(400).json({ message });
+  }
+
+  const url = `${IEX_CLOUD.BASE_URL}/stock/market/list/${type}?token=${IEX_CLOUD.API_KEY}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    res.json(data);
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.get('/market/indices', async (_, res, next) => {
   try {
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 export const IEX_CLOUD = {
   API_KEY: process.env.IEX_CLOUD_API_KEY,
-  BASE_URL: process.env.IEX_CLOUD_BASE_URL,
+  BASE_URL: 'https://cloud.iexapis.com/stable',
   SECRET: process.env.IEX_CLOUD_SECRET,
 };
 
