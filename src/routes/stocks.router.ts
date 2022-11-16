ef4b03746ed54816796e0cdd957cb2f43cd28a59
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2022-11-15 21:11:50 -0600

    feat: add symbol and chart endpoints

--- src/routes/stocks.router.ts
+++ src/routes/stocks.router.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { IEX_CLOUD, MARKET_INDEX_URL } from '../utils/constants';
 import fetch from 'node-fetch';
+import { nextTick } from 'process';
 
 const router = express.Router();
 
@@ -53,6 +54,33 @@ router.get('/market/indices', async (_, res, next) => {
     const response = await fetch(`${MARKET_INDEX_URL}/api/v2/indices`);
     let data = await response.json();
 
+    res.status(200).json(data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// GET /api/stocks/:symbol
+router.get('/:symbol', async (req, res, next) => {
+  try {
+    const symbol = req.params.symbol.toUpperCase();
+    const url = `${IEX_CLOUD.BASE_URL}/stock/${symbol}/batch?types=quote,company,intraday-prices,stats&token=${IEX_CLOUD.API_KEY}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// GET /api/stocks/:symbol/chart/:range
+router.get('/:symbol/chart/:range', async (req, res, next) => {
+  try {
+    const symbol = req.params.symbol.toUpperCase();
+    const range = req.params.range;
+    const url = `${IEX_CLOUD.BASE_URL}/stock/${symbol}/chart/${range}?token=${IEX_CLOUD.API_KEY}`;
+    const response = await fetch(url);
+    const data = await response.json();
     res.status(200).json(data);
   } catch (err) {
     next(err);
